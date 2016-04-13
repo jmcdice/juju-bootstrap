8e06dbb798230a810c5e5da1bbd4a4c53b5be135
Joey <joey.mcdonald@nokia.com>
2016-04-13 04:30:46 +0000

    One last spot changing config-get.

--- heat/destroy.sh
+++ heat/destroy.sh
@@ -3,11 +3,11 @@
 # This should deploy our heat stack and include all the stuff we 
 # need to know about.
 
-OS_USERNAME=$(charm-get os_username)
-OS_TENANT_NAME=$(charm-get os_tenant_name)
-OS_PASSWORD=$(charm-get os_password)
-KEYSTONE_ADMIN_TOKEN=$(charm-get keystone_admin_token)
-OS_AUTH_URL=$(charm-get os_auth_url)
+OS_USERNAME=$(config-get os_username)
+OS_TENANT_NAME=$(config-get os_tenant_name)
+OS_PASSWORD=$(config-get os_password)
+KEYSTONE_ADMIN_TOKEN=$(config-get keystone_admin_token)
+OS_AUTH_URL=$(config-get os_auth_url)
 
 heat stack-destroy epdg-stack-00
 
